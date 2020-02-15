b11fe5b9fbbea0828f0933ad2825d7282cef4e43
aliemtakarai <aliemtakarai@gmail.com>
2020-02-15 14:59:11 +0800

    add assets for layout and component

--- public/js/app.js
+++ public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/aliemtakarai/Work/takarai/japan-test/yarakuzen-test/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/aliemtakarai/Work/takarai/japan-test/yarakuzen-test/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/aliemtakarai/Work/takarai/yarakuzen-laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/aliemtakarai/Work/takarai/yarakuzen-laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
